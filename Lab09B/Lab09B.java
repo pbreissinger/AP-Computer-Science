1c4f8aabce03c0b34ae63c9f4a9a88fd5a61783d
Peter Breissinger <pbreissinger@gmail.com>
2016-10-11 10:08:01 -0400

    Update Lab09B.java

--- Lab09B/Lab09B.java
+++ Lab09B/Lab09B.java
@@ -1,8 +1,3 @@
-// This is the student, starting version of the Text Lab 05 assignment.
-// There are 5 return methods in the Rational class that have temporary return statements
-// which allow the program to compile.  Students will need to change these statements.
-
-
 import java.util.Scanner;
 
 
@@ -20,15 +15,15 @@ public class Lab09B
         Rational r3 = new Rational();
 
         r3.multiply(r1,r2);
-        System.out.println("\n\n" + r1.getOriginal() + " * " + r2.getOriginal() + "  =  " + r3.getRational());
+        System.out.println("\n\n" + r1.getOriginal() + " * " + r2.getOriginal() + "  =  " + r3.getReduced());
         r3.divide(r1,r2);
-        System.out.println("\n" + r1.getOriginal() + " / " + r2.getOriginal() + "  =  " + r3.getRational());
+        System.out.println("\n" + r1.getOriginal() + " / " + r2.getOriginal() + "  =  " + r3.getReduced());
 
-        //      100 Point Version Only
-        r3.add(r1,r2);
-        System.out.println("\n" + r1.getOriginal() + " + " + r2.getOriginal() + "  =  " + r3.getRational());
+        // 100 Point Version Only
+        r3.addition(r1,r2);
+        System.out.println("\n" + r1.getOriginal() + " + " + r2.getOriginal() + "  =  " + r3.getReduced());
         r3.subtract(r1,r2);
-        System.out.println("\n" + r1.getOriginal() + " - " + r2.getOriginal() + "  =  " + r3.getRational());
+        System.out.println("\n" + r1.getOriginal() + " - " + r2.getOriginal() + "  =  " + r3.getReduced());
         System.out.println();
     }
 
@@ -49,9 +44,10 @@ public class Lab09B
 
 class Rational
 {
-
-    private int num, den;    
-    private int firstNum, firstDen;    // for getRational
+    private int firstNum;
+    private int firstDen;
+    private int num;
+    private int den;
     
     public Rational() 
     { 
@@ -63,7 +59,7 @@ class Rational
         this.den = den;
         firstNum = num;
         firstDen = den;
-        reduce();
+        reduced();
     }
 
     public int getNum()
@@ -76,12 +72,7 @@ class Rational
         return firstDen;
     }
 
-    public double getDecimal()
-    { 
-        return (double) firstNum/firstDen;
-    }
-
-    public String getRational()
+    public String getReduced()
     { 
         return num + "/" + den;
     }
@@ -90,15 +81,43 @@ class Rational
     { 
         return firstNum + "/" + firstDen;
     }
-
-    public void reduce()
+    
+    public void reduced()
     {
         firstNum = num;
         firstDen = den;
-        num = firstNum/getGCF(firstNum,firstDen);
-        den = firstDen/getGCF(firstNum,firstDen);
+        num = firstNum/getGCF (firstNum, firstDen);
+        den = firstDen/getGCF (firstNum, firstDen);
+    }
+    
+        public void multiply(Rational r1, Rational r2)
+    {
+        num = r1.getNum() * r2.getNum();
+        den = r1.getDen() * r2.getDen();
+        reduced();
+    }
+    
+    public void divide(Rational r1, Rational r2)
+    {
+        num = r1.getNum() * r2.getDen();
+        den = r1.getDen() * r2.getNum();
+        reduced();
     }
 
+    public void addition(Rational r1, Rational r2)
+    {
+        den = r1.getDen() * r2.getDen();
+        num = (r1.getNum() * r2.getDen()) + (r2.getNum() * r1.getDen());
+        reduced();
+    }
+
+    public void subtract(Rational r1, Rational r2)
+    {
+        den = r1.getDen() * r2.getDen();
+        num = (r1.getNum() * r2.getDen()) - (r2.getNum() * r1.getDen());
+        reduced();
+    }
+    
     private int getGCF(int n1,int n2)
     {
         int rem = 0;
@@ -117,33 +136,4 @@ class Rational
         while (rem != 0);
         return gcf;
     }
-    
-    public void multiply(Rational r1, Rational r2)
-    {
-        num = r1.getNum() * r2.getNum();
-        den = r1.getDen() * r2.getDen();
-        reduce();
-    }
-
-    public void divide(Rational r1, Rational r2)
-    {
-        num = r1.getNum() * r2.getDen();
-        den = r1.getDen() * r2.getNum();
-        reduce();
-    }
-
-    public void add(Rational r1, Rational r2)
-    {
-        den = r1.getDen() * r2.getDen();
-        num = (r1.getNum() * r2.getDen()) + (r2.getNum() * r1.getDen());
-        reduce();
-    }
-
-    public void subtract(Rational r1, Rational r2)
-    {
-        den = r1.getDen() * r2.getDen();
-        num = (r1.getNum() * r2.getDen()) - (r2.getNum() * r1.getDen());
-        reduce();
-    }
-
 }
