74a2661bac8a07aee17009c4068a7ccc06ccb737
Peter Breissinger <pbreissinger@gmail.com>
2016-12-14 13:49:37 -0500

    Update Lab11avst.java

--- lab11a/Lab11avst.java
+++ lab11a/Lab11avst.java
@@ -6,32 +6,56 @@
 
 
 import java.util.Scanner;
-
+import java.text.DecimalFormat;
 
 public class Lab11avst
 {
-	public static void main(String[] args)
-	{
-		// This main method needs additions for the 100 point version.
-		Scanner input = new Scanner(System.in);
-		final int MAX = 100;
-		boolean primes[];
-		primes = new boolean[MAX];
-		computePrimes(primes);
-		displayPrimes(primes);
-	}
+    public static void main(String[] args)
+    {
+        // This main method needs additions for the 100 point version.
+        Scanner input = new Scanner(System.in);
+        System.out.print ("Enter the primes upper bound ===>>  ");
+        final int MAX = input.nextInt ();
+        boolean primes[] = new boolean[MAX];
+        computePrimes(primes);
+        displayPrimes(primes);
+    }
 
-	public static void computePrimes(boolean primes[])
-	{
-		System.out.println("\nCOMPUTING PRIME NUMBERS");
+    public static void computePrimes(boolean primes[])
+    {
+        System.out.println("\nCOMPUTING PRIME NUMBERS");
+        for (int k = 2; k < primes.length; k++)
+            primes[k] = true;
+            
+        for (int p = 2; p < primes.length; p++)
+            for (int q = p * p; q < primes.length; q += p)
+               primes [q] = false;
+    }
 
-	}
-
-	public static void displayPrimes(boolean primes[])
-	{
-		System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
-		System.out.println();
-
-	}
-
+    public static void displayPrimes(boolean primes[])
+    {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
+        System.out.println();
+        boolean prime = true;
+        int x = 0;
+        int y = 0;
+        for (int number = 0; number < primes.length; number++)
+        {
+            if (y % 15 == 0 && prime != false)
+            {
+                System.out.println();
+                prime = false;
+            }
+            
+            if (primes[number] == true)
+            {
+                x = number;
+                DecimalFormat dformat = new DecimalFormat("0000");
+                String Formatted = dformat.format(x);
+                System.out.print(Formatted + " ");
+                y++;
+                prime = true;
+            }
+        }
+    }
 }
